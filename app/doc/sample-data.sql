12a5263c28988be9dee4792286a3ffab37375bc6
cornchiplick <ilsunkiss@gmail.com>
2022-11-04 15:34:56 +0900

    Doc: add new create-table sql query

--- app/doc/sample-data.sql
+++ app/doc/sample-data.sql
@@ -102,10 +102,12 @@ INSERT INTO jangdb.jang_so_recommendation (RECONO, ID, TITLE, WDATE, CNT, ACT, P
 
 -- jang_so_review
 INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (1, '강원도 정선 여행 2일차. 병방치 스카이워크에 잠깐 들린 후 오늘의 메인 코스인 정선 레일바이크로 향했다. 3년 전에 한번 왔었는데 미리 예약을 하지 못해 돌아갔던 아쉬움을 안고 다시 찾아서인지 설레임이 가득하다.', 1, 1);
-INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (2, '정선 레일바이크 출발지는 구절리역으로, 예전 열차 선로 그대로 보존이 되어 있다. 메뚜기 모양의 카페가 있고 그 뒤로는 열차펜션이 위치해 있다. 시설은 좋아보이지 않으나, 새마을호나 무궁화호 등으로 열차여행을 오시는 분들한텐 펜션 이용도 괜찮을 것 같다.', 2, 1);
-INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (3, '화암약수까지 차가 올라갈 수 있어 어찌나 다행이던지... 이 추위에 걸으라 했으면 나는 꼼짝없이 동장군이 되어 굳어 버렸을거다. 심지어 정선 여행 내내 너무 추워 기억의 절반은 날아간 것 같은 기분이다.', 3, 2);
-INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (4, '강원도 국가지질공원 코스는 화암 동굴! 지질명소 중 꽤 유명한 곳으로 통하고 있으며 카르스트지형으로 석회암의 용식 작용으로 만들어진 곳이다.', 4, 3);
-INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (5, '화암 동굴로 향하는 모노레일을 탑승하고 정상으로 올랐다. 참고로 모노레일과 입장권을 예약할 때 할인되는 항목이 있다면 꼼꼼하게 챙기시길! 모이면 생각보다 꽤 많은 금액이 세이브되곤 한다.', 5, 3);
+INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (2, '화암약수까지 차가 올라갈 수 있어 어찌나 다행이던지... 이 추위에 걸으라 했으면 나는 꼼짝없이 동장군이 되어 굳어 버렸을거다. 심지어 정선 여행 내내 너무 추워 기억의 절반은 날아간 것 같은 기분이다.', 1, 2);
+INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (3, '강원도 국가지질공원 코스는 화암 동굴! 지질명소 중 꽤 유명한 곳으로 통하고 있으며 카르스트지형으로 석회암의 용식 작용으로 만들어진 곳이다.', 1, 3);
+INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (4, '정선 레일바이크 출발지는 구절리역으로, 예전 열차 선로 그대로 보존이 되어 있다. 메뚜기 모양의 카페가 있고 그 뒤로는 열차펜션이 위치해 있다. 시설은 좋아보이지 않으나, 새마을호나 무궁화호 등으로 열차여행을 오시는 분들한텐 펜션 이용도 괜찮을 것 같다.', 2, 1);
+INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (5, '화암약수까지 차가 올라갈 수 있어 어찌나 다행이던지... 이 추위에 걸으라 했으면 나는 꼼짝없이 동장군이 되어 굳어 버렸을거다. 심지어 정선 여행 내내 너무 추워 기억의 절반은 날아간 것 같은 기분이다.', 3, 2);
+INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (6, '강원도 국가지질공원 코스는 화암 동굴! 지질명소 중 꽤 유명한 곳으로 통하고 있으며 카르스트지형으로 석회암의 용식 작용으로 만들어진 곳이다.', 4, 3);
+INSERT INTO jangdb.jang_so_review (PRVNO, CONT, RECONO, PLNO) VALUES (7, '화암 동굴로 향하는 모노레일을 탑승하고 정상으로 올랐다. 참고로 모노레일과 입장권을 예약할 때 할인되는 항목이 있다면 꼼꼼하게 챙기시길! 모이면 생각보다 꽤 많은 금액이 세이브되곤 한다.', 5, 3);
 
 -- jang_so_review_file
 INSERT INTO jangdb.jang_so_review_file (RECOFNO, PRVNO, PATH, FNAME) VALUES (1, 1, '../review_file/file1_1', '파일6');
